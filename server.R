a682c4bb29d00f8441b28b1d8e08bd59f5223256
CyprienF <floquet.cyprien@gmail.com>
2018-01-06 17:50:59 +0100

    Ajout des commentaires

--- server.R
+++ server.R
@@ -9,6 +9,7 @@ library(shiny)
 
 shinyServer(function(input, output) {
   
+  # Variables de la loi binomiale
   obs <- reactive({
     obs <- seq(1,input$nb,1)
   })
@@ -17,6 +18,7 @@ shinyServer(function(input, output) {
     x <- rbinom(obs(),input$n,input$proba)
   })
   
+  # Variable de la loi de poisson
   N <- reactive({
     N <- seq(1,input$nbPoisson,1)
   })
@@ -25,6 +27,7 @@ shinyServer(function(input, output) {
     x <- rpois(N(), input$lambdaPoisson)
   })
 
+  # Graphique de la loi binomiale
   output$dispBinomiale <- renderPlot({
         hist(xBinom(),probability = TRUE,
              xlim=c(min(xBinom()),max(xBinom())), nclass=max(xBinom())-min(xBinom())+1, 
@@ -34,18 +37,22 @@ shinyServer(function(input, output) {
         
   })
   
+  # Résumé de la loi binomiale
   output$summaryBinomiale <- renderPrint({
     summary(xBinom())
   })
   
+  # Table des valeurs de la loi binomiale
   output$tableBino <- renderTable({
     summary <- table(xBinom())
   })
   
+  # Boxplot de la loi binomiale
   output$boxplotBinomiale <- renderPlot({
     boxplot(xBinom())
   })
   
+  # Graphique de la loi de poisson
   output$dispPoisson <- renderPlot({
     hist(xPoiss(), 
          xlim=c(min(xPoiss()),max(xPoiss())), probability=T, nclass=max(xPoiss())-min(xPoiss())+1, 
@@ -54,18 +61,22 @@ shinyServer(function(input, output) {
     lines(density(xPoiss(),bw=1), col='red', lwd=3)
   })
   
+  # Résumé de la loi de poisson
   output$summaryPoisson <- renderPrint({
     summary(xPoiss())
   })
   
+  # Table des valeurs de la loi de poisson
   output$tablePois <- renderTable({
     summary <- table(xPoiss())
   })
   
+  # Boxplot de la loi de poisson
   output$boxplotPoisson <- renderPlot({
     boxplot(xPoiss())
   })
   
+  # Téléchargement du fichier de la loi binomiale
   output$downloadData <- downloadHandler(
     filename = function() {
       paste("table_valeurs_binom", ".csv", sep = "")
@@ -75,6 +86,7 @@ shinyServer(function(input, output) {
     }
   )
   
+  # Téléchargement du fichier de la loi de poisson
   output$downloadData1 <- downloadHandler(
     filename = function() {
       paste("table_valeurs_poiss", ".csv", sep = "")
